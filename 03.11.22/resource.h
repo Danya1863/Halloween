45e860bfaa75f681242bc5521dac84a5f4fa4ad7
Danya <danya978@ukr.net>
2022-11-14 11:28:17 +0200

    Добавьте файлы проекта.

---
+++ 03.11.22/resource.h
@@ -0,0 +1,32 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 03.11.22.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MY031122_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MY031122                    107
+#define IDI_SMALL                       108
+#define IDC_MY031122                    109
+#define IDR_MAINFRAME                   128
+#define IDC_CURSOR1                     129
+#define IDI_ICON1                       133
+#define IDI_ICON2                       134
+#define IDC_CURSOR2                     135
+#define IDC_CURSOR3                     136
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
